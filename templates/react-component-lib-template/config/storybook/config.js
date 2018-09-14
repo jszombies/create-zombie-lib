6a859ac65e8a6234245ba7dcc946f6997cccef0a
Astma <aleh.atsman@gmail.com>
2018-09-14 10:44:08 +0200

    Load stories automatically

--- templates/react-component-lib-template/config/storybook/config.js
+++ templates/react-component-lib-template/config/storybook/config.js
@@ -1,7 +1,9 @@
 import { configure } from '@storybook/react';
 
+const req = require.context('../../src', true, /\.stories\.js.$/);
+
 function loadStories() {
-  require('../../src/Example.story.jsx');
+  req.keys().forEach(filename => req(filename));
 }
 
 configure(loadStories, module);
